2836638a7922ac4b82799ea2590204a59d379f14
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2020-08-06 17:07:22 +0900

    Split example code from library code

---
+++ example/index.ts
@@ -0,0 +1,9 @@
+import { drawShader } from '../src'
+import { program } from './log-polar'
+
+const canvas = document.querySelector('canvas') as HTMLCanvasElement
+const gl = canvas.getContext('webgl')
+
+const draw = drawShader(program)
+window.addEventListener('resize', () => draw(gl))
+draw(gl)
---
+++ example/log-polar.ts
@@ -0,0 +1,54 @@
+import {
+  SDF,
+  Program,
+  composeM,
+  circle,
+  box,
+  polygon,
+  union,
+  dilate,
+  outline,
+  translate,
+  rotate,
+  scale,
+  mirrorX,
+  mirrorY,
+  repeatPolar,
+  repeatLogPolar,
+  invert,
+  stripeRamp,
+} from '../src'
+
+const TAU = Math.PI * 2
+
+const circles = outline(15)(
+  rotate(TAU / 8)(
+    mirrorY(mirrorX(scale(2)(translate({ x: 100, y: 100 })(circle(50)))))
+  )
+)
+const cross = rotate(TAU / 8)(
+  dilate(5)(union(box({ x: 300, y: 20 }), box({ x: 20, y: 300 })))
+)
+const poly = invert(
+  rotate(TAU / 8)(
+    dilate(400)(
+      polygon([
+        { x: -100, y: -100 },
+        { x: 100, y: -100 },
+        { x: 100, y: 100 },
+        { x: -100, y: 100 },
+      ])
+    )
+  )
+)
+
+const pattern = rotate(TAU / 20)(
+  scale(0.35)(
+    repeatPolar(5)(
+      translate({ x: 800, y: 0 })(union(poly, union(cross, circles)))
+    )
+  )
+)
+const sdf: SDF = repeatLogPolar(12)(scale(0.001)(pattern))
+
+export const program: Program = composeM(sdf)(stripeRamp)
